33449a78ac2f18c8ef217a434c5d66642753588f
Andrey Miroshnikov <am@redsemiconductor.com>
2023-12-05 12:24:30 +0000

    Working Horizontal-First version of memrchr

--- svp64-port/svp64/memrchr_svp64.s
+++ svp64-port/svp64/memrchr_svp64.s
@@ -17,6 +17,10 @@
     .set s, 16
     .set t, 32
     .set rem, 11
+    .set addr0, 12
+    .set addr1, 13
+    .set addr2, 14
+    .set addr3, 15
 
 # Helper macros
 
@@ -90,36 +94,55 @@ __memchr:
 
 .skip_offset:
     # set up ctr to 4 64-bit elements (32 bytes)
-    li                  ctr, 1 # using 1 for now, was 4
-    mtctr               ctr
-    setvl               0, ctr, 4, 1, 1, 1      # Set VL to 4 elements, VF=1
+    li                  ctr, 4 # using 1 for now, was 4
+    li                  tmp, 5 # needs to be ctr+1, so that sv.bc will branch with CTR>0
 .inner:
-    sv.ld               *s, 0(in_ptr)           # Load from *in_ptr
+    # Create local in_ptr copies in reverse for Horizontal-First
+    mr                  addr0, in_ptr
+    setvl               0, 0, 3, 0, 1, 1 # VL=3
+    # binutils doesn't support the subi extended mnemonics yet
+    sv.addi             *addr1, *addr0, -8
+
+    setvl               0, ctr, 4, 0, 1, 1      # Set VL to 4 elements, VF=1
+    sv.ld               *s, 0(*addr0)           # Load from *in_ptr
     sv.cmpb             *t, *s, c64             # this will create a bitmask of FF where character c is found
     sv.cmpi             *cr0, 1, *t, 0
-    # Hard-coded instead of .mov_foward_by_7, binutils calculated
-    # the wrong address of 0x94, which is bc instruction,
+    # Counter needed for sv.bc, allows to determine which
+    # doubleword has a matching char
+    mtctr               tmp
+    # Hard-coded instead of .determine_loc, binutils calculated
+    # the wrong address of 0xa4, which is bc instruction,
     # so get's stuck in infinite loop
     # To calculate the right constant, do make all,
     # then powerpc64le-linux-gnu-objdump -D svp64/memrchr_svp64.o
     # To see the dump of the object file.
-    # Take the address you want to got (i.e. 0xb4), subtract
-    # the starting address of sv prefix for sv.bc (0x90),
-    # the difference is the value to use (0xb4-0x90=0x24).
-    #sv.bc               4, *2, .mov_foward_by_7
-    sv.bc               4, *2, 0x24
-    svstep              0, 1, 0
-    subi                in_ptr, in_ptr, 8
-    subi                n, n, 8
-    #sv.bc/all           16, *cr0, .found
-    # alternatively could checked the overflow bit of CR0...
-    bdnz                .inner # or alternatively, bc 16, 0, .inner
+    # Take the address you want to get to (i.e. 0xbc), subtract
+    # the starting address of sv prefix for sv.bc (0xa0),
+    # the difference is the value to use (0xbc-0xa0=0x1c).
+    #sv.bc               0, *2, .determine_loc
+    sv.bc               0, *2, 0x1c
+    subi                in_ptr, in_ptr, 32
+    subi                n, n, 32
 
     # If n is now less than 32, need to mov in_ptr forward by 7
     cmpldi              n, 32
     blt                 .mov_foward_by_7
 
     b                   .outer
+
+# At this point we know the matched character is within 32-bytes
+# Now need to find which 8-byte doubleword.
+.determine_loc:
+    mfspr               ctr, 9 # Read current value of CTR reg
+    li                  tmp, 4
+    # 4-ctr will give the number of times 8 needs to be subtracted
+    # from in_ptr to get to the doubleword with matched char.
+    sub                 tmp, tmp, ctr
+    mtctr               tmp
+.sub_in_ptr:
+    subi                in_ptr, in_ptr, 8
+    subi                n, n, 8
+	bdnz                .sub_in_ptr
 
 # At the start, before SVP64 code, in_ptr was moved back by
 # 7 bytes to read the doubleword (64-bits/8 bytes) correctly.
