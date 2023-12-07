9378006a84bdef6af85eb0f810fb62fedc62c588
Andrey Miroshnikov <am@redsemiconductor.com>
2023-12-07 01:45:22 +0000

    strchr: Adding strchr function

---
+++ svp64-port/strchr_wrapper.c
@@ -0,0 +1,171 @@
+/* Copyright (C) 1991-2018 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+   Based on strlen implementation by Torbjorn Granlund (tege@sics.se),
+   with help from Dan Sahlin (dan@sics.se) and
+   commentary by Jim Blandy (jimb@ai.mit.edu);
+   adaptation to memchr suggested by Dick Karpinski (dick@cca.ucsf.edu),
+   and implemented by Roland McGrath (roland@ai.mit.edu).
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#ifndef _LIBC
+# include <config.h>
+#endif
+
+#include <string.h>
+
+#include <stddef.h>
+
+#include <limits.h>
+
+#undef __strchr_svp64
+#ifdef _LIBC
+# undef strchr_svp64
+#endif
+
+#ifndef weak_alias
+# define __strchr_svp64 strchr_svp64
+#endif
+
+#ifndef STRCHR_SVP64
+# define STRCHR_SVP64 __strchr_svp64
+#endif
+
+#include <Python.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#include "pypowersim_wrapper_common.h"
+
+const char *strchr_svp64_filename = "./bin/strchr_svp64.bin";
+
+/* Search S for C until NULL string terminator encountered.  */
+char*
+STRCHR_SVP64 (const char *s, int c)
+{
+    #define REG_s 3 // Set GPR #3 to the pointer s
+    #define REG_c 4 // Set GPR #4 to the char 'c'
+
+    printf("strchr_svp64 called: s: %p, c: %02x(%c)\n", s, (uint8_t)c, c);
+
+    const char *sptr = s;
+    // These cannot be the same pointer as the original function, as it is really a separate CPU/RAM
+    // we have to memcpy from input to this pointer, the address was chosen arbitrarily
+    uint64_t sptr_svp64  = 0x100000;
+
+    // Create the pypowersim_state
+    pypowersim_state_t *state = pypowersim_prepare();
+
+    // Change the relevant elements, mandatory: body
+    state->binary = PyBytes_FromStringAndSize(strchr_svp64_filename, strlen(strchr_svp64_filename));
+    // Set GPR #REG_s to the pointer s
+    PyObject *s_address = PyLong_FromUnsignedLongLong(sptr_svp64);
+    PyList_SetItem(state->initial_regs, REG_s, s_address);
+
+    // Set GPR #REG_c to the char 'c'
+    PyObject *c_svp64 = PyLong_FromUnsignedLongLong(c);
+    PyList_SetItem(state->initial_regs, REG_c, c_svp64);
+
+    // Load data into buffer from real memory
+    size_t bytes = strlen(s);
+    size_t bytes_rem = bytes % 8;
+    bytes -= bytes_rem;
+    printf("bytes: %ld, bytes_rem: %ld\n", bytes, bytes_rem);
+    
+    uint64_t svp64_ptr = sptr_svp64;
+    // Load data into buffer from real memory
+    for (size_t i=0; i < bytes; i += 8) {
+      PyObject *svp64_address = PyLong_FromUnsignedLongLong(svp64_ptr);
+      uint64_t *sptr64 = (uint64_t *) sptr;
+      /*printf("m[%ld] \t: %p -> %02x %02x %02x %02x %02x %02x %02x %02x\n", i, sptr64, sptr[0], sptr[1], sptr[2], sptr[3],
+                                                                             sptr[4], sptr[5], sptr[6], sptr[7]);
+      printf("val \t: %016lx -> %016lx\n", *sptr64, svp64_ptr);*/
+      PyObject *word = PyLong_FromUnsignedLongLong(*sptr64);
+      PyDict_SetItem(state->initial_mem, svp64_address, word);
+      sptr += 8;
+      svp64_ptr += 8;
+    }
+    // Load remaining bytes
+    PyObject *svp64_address = PyLong_FromUnsignedLongLong(svp64_ptr);
+    uint64_t sptr64 = 0;
+    uint8_t *sptr8 = (uint8_t *) &sptr64;
+    for (size_t i=0; i < bytes_rem; i++) {
+        sptr8[i] = sptr[i];
+        printf("%02x ", sptr[i]);
+    }
+    printf("\n");
+    //printf("val \t: %016lx -> %016lx\n", sptr64, svp64_ptr);
+    PyObject *word = PyLong_FromUnsignedLongLong(sptr64);
+    PyDict_SetItem(state->initial_mem, svp64_address, word);
+
+    // Prepare the arguments object for the call
+    pypowersim_prepareargs(state);
+
+    // Call the function and get the resulting object
+    state->result_obj = PyObject_CallObject(state->simulator, state->args);
+    if (!state->result_obj) {
+        PyErr_Print();
+        printf("Error invoking 'run_a_simulation'\n");
+        pypowersim_finalize(state);
+        exit(1);
+    }
+
+    // Get the GPRs from the result_obj
+    PyObject *final_regs = PyObject_GetAttrString(state->result_obj, "gpr");
+    if (!final_regs) {
+        PyErr_Print();
+        printf("Error getting final GPRs\n");
+        pypowersim_finalize(state);
+        exit(1);
+    }
+
+    // GPR #REG_s holds the return value as an integer
+    PyObject *key = PyLong_FromLongLong(REG_s);
+    PyObject *itm = PyDict_GetItem(final_regs, key);
+    if (!itm) {
+        PyErr_Print();
+        printf("Error getting GPR #%d\n", REG_s);
+        pypowersim_finalize(state);
+        exit(1);
+    }
+    PyObject *value = PyObject_GetAttrString(itm, "value");
+    if (!value) {
+        PyErr_Print();
+        printf("Error getting value of GPR #%d\n", REG_s);
+        pypowersim_finalize(state);
+        exit(1);
+    }
+    uint64_t val = PyLong_AsUnsignedLongLong(value);
+    printf("return val \t: %016lx\n", val);
+    if (val) {
+        // Return value
+        char *result = (char *) s;
+        printf("s         : %p\n", result);
+        uint64_t offset = val - sptr_svp64;
+        printf("sptr_svp64: %016lx\n", sptr_svp64);
+        printf("val       : %016lx\n", val);
+        printf("offset    : %016lx\n", offset);
+        result += offset;
+        printf("result    : %p\n", result);
+        return result;
+    } else {
+        return NULL;
+    }
+    pypowersim_finalize(state);
+}
+#ifdef weak_alias
+weak_alias (__strchr_svp64, strchr_svp64)
+#endif
+libc_hidden_builtin_def (strchr_svp64)
---
+++ svp64-port/svp64/strchr_svp64.s
@@ -0,0 +1,143 @@
+    .machine libresoc
+    .file   "strchr.c"
+    .abiversion 2
+    .section   ".text"
+    .align 2
+
+    .set in_ptr, 3
+    .set res, 3
+    .set c, 4
+    .set n, 5
+    .set tmp, 6
+    .set ctr, 7
+    .set c64, 8
+    .set s, 16
+    .set t, 32
+    .set addr0, 12
+    .set addr1, 13
+
+# Helper macros
+
+# load & duplicate byte to a 64-bit register, tmp register passed
+.macro  ldbi rD, rT
+    rldicr  \rT,\rD,8,55
+    or      \rT,\rT,\rD
+    rldicr  \rD,\rT,16,47
+    or      \rD,\rD,\rT
+    rldicr  \rT,\rD,32,31
+    or      \rD,\rT,\rD
+.endm
+
+# Determine the string size - not given with the str* functions
+.macro strlen rN, rS
+    li                  \rN, 0
+    # temp copy of pointer to str for determining length
+    mr                  tmp, \rS
+.check_len:
+    lbz                 s, 0(tmp)
+    cmpi                cr0, 1, s, 0
+    addi                tmp, tmp, 1
+    addi                \rN, \rN, 1
+    bne                 cr0,.check_len
+    # Saves having an extra label, perhaps not the best implementation...
+    # need to have this if first char is NULL, meaning n=0
+    subi                \rN, \rN, 1
+.endm
+
+    .globl __strchr
+    .type   __strchr, @function
+__strchr:
+.LFB6:
+    .cfi_startproc
+
+    # Steps required for strchr
+    # 1. Determine length of string, n
+    # 2. Use the same algorithm for memchr, Horizontal-First version.
+
+    # Load the character c into all bytes of register c64: GPR#7
+    ori                 c64, c, 0
+    ldbi                c64, tmp
+
+    # Determine the length of string
+    strlen              n, in_ptr
+
+.outer:
+    # Simple case: if bytes == 0, return NULL
+    cmpldi              n, 0
+    beq                 .tail
+
+    # We should check for n <= 32
+    cmpldi              n, 32
+    blt                 .found
+
+    # set up ctr to 4 64-bit elements (32 bytes)
+    li                  ctr, 4 # using 1 for now, was 4
+    li                  tmp, 5 # needs to be ctr+1, so that sv.bc will branch with CTR>0
+.inner:
+    # Create local in_ptr copies for Horizontal-First
+    mr                  addr0, in_ptr
+    setvl               0, 0, 3, 0, 1, 1 # VL=3
+    sv.addi             *addr1, *addr0, 8
+
+    setvl               0, ctr, 4, 0, 1, 1      # Set VL to 4 elements
+    sv.ld               *s, 0(*addr0)           # Load from *in_ptr
+    sv.cmpb             *t, *s, c64             # this will create a bitmask of FF where character c is found
+    sv.cmpi             *cr0, 1, *t, 0
+    # Counter needed for sv.bc, allows to determine which
+    # doubleword has a matching char
+    mtctr               tmp
+    # Hard-coded instead of .determine_loc, binutils calculated
+    # the wrong address of 0xa4, which is bc instruction,
+    # so get's stuck in infinite loop
+    # To calculate the right constant, do make all,
+    # then powerpc64le-linux-gnu-objdump -D svp64/memrchr_svp64.o
+    # To see the dump of the object file.
+    # Take the address you want to get to (i.e. 0xbc), subtract
+    # the starting address of sv prefix for sv.bc (0xa0),
+    # the difference is the value to use (0xbc-0xa0=0x1c).
+    #sv.bc               0, *2, .determine_loc
+    sv.bc               0, *2, 0x1c
+    addi                in_ptr, in_ptr, 32
+    subi                n, n, 32
+
+    b                   .outer
+
+# At this point we know the matched character is within 32-bytes
+# Now need to find which 8-byte doubleword.
+.determine_loc:
+    mfspr               ctr, 9 # Read current value of CTR reg
+    li                  tmp, 4
+    # 4-ctr will give the number of times 8 needs to be added
+    # to in_ptr to get to the doubleword with matched char.
+    sub                 tmp, tmp, ctr
+    cmpi                cr0, 1, tmp, 0
+    beq                 cr0, .found
+    mtctr               tmp
+.add_in_ptr:
+    addi                in_ptr, in_ptr, 8
+    #subi                n, n, 8
+    bdnz                .add_in_ptr
+
+.found:
+    mtctr	            n                       # Set up counter
+.found2:
+    lbz                 s, 0(in_ptr)
+    cmpw                cr0, c, s
+    beqlr               cr0
+    addi                in_ptr, in_ptr, 1
+    bdnz                .found2
+
+.tail:
+    # If we have reached this point, there is no match, return NULL
+    li                  res, 0
+    blr
+    .long 0
+    .byte 0,0,0,0,0,0,0,0
+    .cfi_endproc
+.LFE6:
+    .size   __strchr,.-__strchr
+    .weak   strchr
+    .hidden strchr
+    .set    strchr,__strchr
+    .ident  "GCC: (Debian 8.3.0-2) 8.3.0"
+    .section    .note.GNU-stack,"",@progbits
